95d2815065d11d5eea0a27088a427c87ce2663e7
John Brewer <brewerja@gmail.com>
2018-08-15 21:02:59 -0400

    GameParser class creation

--- parse_all.py
+++ parse_all.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from bs4 import BeautifulSoup
 
-from parse_game import parse_game
+from parse_game import GameParser
 from parse_players import PlayersParser
 
 GD2_URL = 'https://gd2.mlb.com/components/game/mlb'
@@ -37,9 +37,12 @@ def get_todays_game_ids():
 
 if __name__ == '__main__':
     game_ids = list_game_ids(2018, 8, 11)
+    event_types = set()
     for game_id in game_ids:
         game_url = get_game_url(game_id)
         players = PlayersParser(get(game_url + 'players.xml')).players
         for p in players:
             print('%d %s. %s' % (p.id, p.first[0], p.last))
-        parse_game(get(game_url + 'inning/inning_all.xml'))
+        g = GameParser(get(game_url + 'inning/inning_all.xml'))
+        event_types = event_types.union(g.event_types)
+    print(event_types)
--- parse_game.py
+++ parse_game.py
@@ -1,57 +1,58 @@
 import xml.etree.ElementTree as ET
 
+class GameParser:
+    def __init__(self, xml):
+        self.event_types = set()
+        self.parse_game(xml)
 
-def parse_game(xml):
-    game = ET.fromstring(xml)
+    def parse_game(self, xml):
+        game = ET.fromstring(xml)
 
-    for inning in game:
-        parse_inning(inning)
+        for inning in game:
+            self.parse_inning(inning)
 
+    def parse_inning(self, inning):
+        next = 'N' if inning.attrib['next'] == 'N' else ''
+        print('Inning: ' + inning.attrib['num'] + ' ' + next)
 
-def parse_inning(inning):
-    next = 'N' if inning.attrib['next'] == 'N' else ''
-    print('Inning: ' + inning.attrib['num'] + ' ' + next)
+        for half_inning in inning:
+            self.parse_half_inning(half_inning)
 
-    for half_inning in inning:
-        parse_half_inning(half_inning)
+    def parse_half_inning(self, half_inning):
+        print(' - ' + half_inning.tag)
 
+        for step in half_inning:
+            self.parse_step(step)
 
-def parse_half_inning(half_inning):
-    print(' - ' + half_inning.tag)
-
-    for step in half_inning:
-        parse_step(step)
-
-
-def parse_step(step):
-    if step.tag == 'atbat':
-        parse_atbat(step)
-    elif step.tag == 'action':
-        parse_action(step)
-    else:
-        raise Exception('Unknown step type')
-
-
-def parse_atbat(atbat):
-    print('  - %s (%s) %s' % (atbat.tag, atbat.attrib['event_num'],
-                              atbat.attrib['des']))
-    for child in atbat:
-        if child.tag == 'runner':
-            parse_runner(child)
-        elif child.tag in ['pitch', 'po']:
-            pass
+    def parse_step(self, step):
+        self.event_types.add(step.attrib['event'])
+        if step.tag == 'atbat':
+            self.parse_atbat(step)
+        elif step.tag == 'action':
+            self.parse_action(step)
         else:
-            raise Exception('Unknown atbat step type: %s' % child.tag)
+            raise Exception('Unknown step type')
 
+    def parse_atbat(self, atbat):
+        print('  - %s (%s) %s' % (atbat.tag, atbat.attrib['event_num'],
+                                  atbat.attrib['des']))
+        for child in atbat:
+            if child.tag == 'runner':
+                self.parse_runner(child)
+            elif child.tag in ['pitch', 'po']:
+                pass
+            else:
+                raise Exception('Unknown atbat step type: %s' % child.tag)
 
-def parse_action(action):
-    print('  - %s (%s) %s' % (action.tag, action.attrib['event_num'],
-                              action.attrib['des']))
+    def parse_action(self, action):
+        print('  - %s (%s) %s' % (action.tag, action.attrib['event_num'],
+                                  action.attrib['des']))
 
-
-def parse_runner(runner):
-    print('   - ' + runner.tag + ' ' + str(runner.attrib))
+    def parse_runner(self, runner):
+        print('   - ' + runner.tag + ' ' + str(runner.attrib))
 
 
 if __name__ == '__main__':
-    parse_game('inning_all.xml')
+    with open('inning_all.xml') as f:
+        g = GameParser(f.read())
+    print(g.event_types)
