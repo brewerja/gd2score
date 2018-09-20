fce89b79f6c0ccc7b60bb3b382db96113785e566
John Brewer <brewerja@gmail.com>
2018-09-19 22:48:51 -0400

    Move RunnerHighlighter class to a new file

--- enhance.py
+++ enhance.py
@@ -5,6 +5,7 @@ from itertools import chain
 from scoring import get_scoring
 from models import Runner
 from parse_players import Player
+from runner_highlighter import RunnerHighlighter
 
 BASE_NUMBER = {
     '1st': 1,
@@ -222,33 +223,3 @@ class GameEnhancer:
 
         for half_inning in half_innings:
             highlighter.highlight(half_inning)
-
-
-class RunnerHighlighter:
-    def highlight(self, half_inning):
-        self.bases_scored_from = []
-        for atbat in reversed(half_inning):
-            self.bases_new = []
-            self.parse_atbat_ending_runners(atbat)
-            self.parse_mid_pa_runners(atbat)
-            self.bases_scored_from = self.bases_new
-
-    def parse_atbat_ending_runners(self, atbat):
-        for runner in atbat.runners:
-            if ((runner.end in self.bases_scored_from or
-                 runner.end == 4) and not runner.out):
-                self.mark_and_save(runner)
-
-    def parse_mid_pa_runners(self, atbat):
-        for runner in sorted(atbat.mid_pa_runners,
-                             key=lambda r: r.event_num, reverse=True):
-            if runner.end in self.bases_new and not runner.out:
-                self.bases_new.remove(runner.end)
-                self.mark_and_save(runner)
-            elif runner.end == 4 and not runner.out:
-                self.mark_and_save(runner)
-
-    def mark_and_save(self, runner):
-        runner.to_score = True
-        if runner.start != 0:
-            self.bases_new.append(runner.start)
---
+++ runner_highlighter.py
@@ -0,0 +1,28 @@
+class RunnerHighlighter:
+    def highlight(self, half_inning):
+        self.bases_scored_from = []
+        for atbat in reversed(half_inning):
+            self.bases_new = []
+            self.parse_atbat_ending_runners(atbat)
+            self.parse_mid_pa_runners(atbat)
+            self.bases_scored_from = self.bases_new
+
+    def parse_atbat_ending_runners(self, atbat):
+        for runner in atbat.runners:
+            if ((runner.end in self.bases_scored_from or
+                 runner.end == 4) and not runner.out):
+                self.mark_and_save(runner)
+
+    def parse_mid_pa_runners(self, atbat):
+        for runner in sorted(atbat.mid_pa_runners,
+                             key=lambda r: r.event_num, reverse=True):
+            if runner.end in self.bases_new and not runner.out:
+                self.bases_new.remove(runner.end)
+                self.mark_and_save(runner)
+            elif runner.end == 4 and not runner.out:
+                self.mark_and_save(runner)
+
+    def mark_and_save(self, runner):
+        runner.to_score = True
+        if runner.start != 0:
+            self.bases_new.append(runner.start)
