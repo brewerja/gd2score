ba9ff61debeced9815e0310b1e77ca9b5d5f2923
John Brewer <brewerja@gmail.com>
2018-10-04 07:52:24 -0400

    Simple test of fix_mid_pa_runners

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,5 +1,6 @@
 import unittest
 
 from .pinch_runners import TestPinchRunnerFixer
+from .mid_pa_runners import TestFixMidPaRunners
 
 unittest.main()
---
+++ tests/mid_pa_runners.py
@@ -0,0 +1,26 @@
+import unittest
+
+from gd2score.enhance import GameEnhancer
+from gd2score.models import Action, AtBat, Runner
+from gd2score.parse_players import Player
+
+
+class TestFixMidPaRunners(unittest.TestCase):
+    def setUp(self):
+        self.enhancer = GameEnhancer()
+        self.enhancer.players = {605141: Player(605141, 'Mookie', 'Betts')}
+
+        action = Action(418, 'Runner Out',
+                        ('Mookie Betts out at 1st, catcher Wilson Ramos to '
+                         'first baseman C. J. Cron.'), 605141)
+        self.ab = AtBat(55, 423, 434670,
+                        'Hanley Ramirez walks.', 'Walk', 643493)
+        self.ab.add_runner(Runner(605141, 1, 0, 418))
+        self.ab.add_runner(Runner(434670, 0, 1, 423))
+        self.ab.add_action(action)
+
+    def test_fix_mid_pa_runners(self):
+        self.enhancer.fix_mid_pa_runners(self.ab)
+        runner_out = self.ab.mid_pa_runners[0]
+        self.assertEqual(runner_out.end, 1)
+        self.assertTrue(runner_out.out)
