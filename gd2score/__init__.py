00728180b4783c316294dbbe5e6464e71813a8a7
John Brewer <brewerja@gmail.com>
2018-10-13 20:39:47 -0400

    Make list_game_ids public

--- gd2score/__init__.py
+++ gd2score/__init__.py
@@ -1,4 +1,5 @@
 from .driver import GameBuilder
 from .draw_scorecard import DrawScorecard
+from .__main__ import list_game_ids
 
 __all__ = ['driver', 'draw_scorecard']
--- gd2score/__main__.py
+++ gd2score/__main__.py
@@ -22,33 +22,29 @@ def list_game_ids(year, month, day):
     return [l for l in links if l.startswith('gid')]
 
 
-def get_todays_game_ids():
-    d = datetime.now()
-    return list_game_ids(d.year, d.month, d.day)
+if __name__ == '__main__':
+    logging.basicConfig(  # filename='parsing.log',
+                        format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
 
+    game_builder = GameBuilder()
+    draw_scorecard = DrawScorecard()
+    start_date = datetime(2016, 5, 20)
+    for date in [start_date + timedelta(days=x) for x in range(0, 200)]:
+        game_ids = list_game_ids(date.year, date.month, date.day)
+        for game_id in game_ids:
+            if game_id in ['gid_2014_04_16_atlmlb_phimlb_1',
+                           'gid_2014_05_23_arimlb_nynmlb_1',
+                           'gid_2014_08_12_arimlb_clemlb_1']:
+                continue
+            try:
+                logging.info('Processing %s', game_id)
+                game = game_builder.build(game_id)
+                drawing = draw_scorecard.draw(game)
+                drawing.saveas('test.svg')
 
-logging.basicConfig(#filename='parsing.log',
-                    format='%(levelname)s:%(message)s',
-                    level=logging.INFO)
-
-game_builder = GameBuilder()
-draw_scorecard = DrawScorecard()
-start_date = datetime(2018, 3, 29)
-for date in [start_date + timedelta(days=x) for x in range(0, 200)]:
-    game_ids = list_game_ids(date.year, date.month, date.day)
-    for game_id in game_ids:
-        if game_id in ['gid_2014_04_16_atlmlb_phimlb_1',
-                       'gid_2014_05_23_arimlb_nynmlb_1',
-                       'gid_2014_08_12_arimlb_clemlb_1']:
-            continue
-        try:
-            logging.info('Processing %s', game_id)
-            game = game_builder.build(game_id)
-            drawing = draw_scorecard.draw(game)
-            drawing.saveas('test.svg')
-
-            #input(game_id)
-        except urllib.error.HTTPError:
-            logging.info('Not found: %s', game_id)
-        except IncompleteGameException:
-            logging.info('Incomplete game: %s', game_id)
+                input(game_id)
+            except urllib.error.HTTPError:
+                logging.info('Not found: %s', game_id)
+            except IncompleteGameException:
+                logging.info('Incomplete game: %s', game_id)
