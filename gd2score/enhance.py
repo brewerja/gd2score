06d8767a3b1fc39479256d8fe3be0dae2e031d32
John Brewer <brewerja@gmail.com>
2019-11-13 21:56:41 -0500

    Remove old parse_game/enhance

--- gd2score/enhance.py
+++ gd2score/enhance.py
@@ -1,34 +1,18 @@
 import re
 import logging
 
+import statsapi
+
 from .models import Runner, Player
 from .runner_highlighter import RunnerHighlighter
-from .pinch_runners import PinchRunnerFixer
-
-BASE_NUMBER = {
-    '1st': 1,
-    '2nd': 2,
-    '3rd': 3,
-    'home': 4
-}
-
-OUTS_ON_BASES = ('(?:out at|doubled off|picked off and caught stealing|'
-                 'caught stealing|was picked off)')
-
-LONG_BASE = '(%s)' % '|'.join(BASE_NUMBER.keys())
-
-PICKS_OFF = 'picks off .* %s at %s'
-
-HITS_IN_PARK = ('Single', 'Double', 'Triple')
 
 
 class GameEnhancer:
     def __init__(self):
         self.runner_highlighter = RunnerHighlighter()
-        self.pinch_runner_fixer = PinchRunnerFixer()
 
     def execute(self, game):
-        self.players = game.players
+        self.players = {}
         self.players[0] = Player(0, 'Held', 'Runner')
 
         for inning in game:
@@ -36,136 +20,36 @@ class GameEnhancer:
                 self.fix_half_inning(half_inning)
                 self.runner_highlighter.highlight(half_inning)
 
+        game.players = self.players
+
     def fix_half_inning(self, half_inning):
-        """Adds a scoring string to each atbat, adds missing runner tags, and
-        resolves the ending bases of runners with empty end tags."""
+        """Adds missing runner tags, and resolves the ending bases of runners
+        with empty end tags."""
         outs = 0
         active_runners = []
         for atbat in half_inning:
-            self.pinch_runner_fixer.fix(atbat, half_inning, self.players)
-            self.fix_mid_pa_runners(atbat)
-            self.resolve_runners(atbat, outs)
+            self.parse_player(atbat.batter)
+            self.parse_player(atbat.pitcher)
+
             self.hold_runners(active_runners, atbat)
 
             active_runners = [r for r in atbat.runners
                               if not r.out and r.end != 4]
             outs = atbat.outs
-            self.display_atbat(atbat)
 
-    def resolve_runners(self, atbat, outs):
-        outs_on_play = (atbat.outs - outs -
-                        sum([1 for r in atbat.mid_pa_runners if r.out]))
-        if atbat.outs != 3:
-            self.resolve_runners_easy(atbat, outs_on_play)
-        else:
-            self.resolve_runners_3outs(atbat, outs_on_play)
-
-    def resolve_runners_easy(self, atbat, outs_on_play):
-        """When there's less than 3 outs, the lack of a runner tag for the
-        batter means that the batter is out. Also, all empty end attributes on
-        runners mean the runner is out."""
-        self.set_runner_out_bases(atbat)
-
-        runner_outs = sum([1 for r in atbat.runners if r.out])
-        batter_runner = [r for r in atbat.runners if r.id == atbat.batter]
-        batter_out = 1 if not batter_runner else 0
-
-        if outs_on_play != runner_outs + batter_out:
-            # Walkoff, runner on base that doesn't score
-            # TODO: Need to handle walkoffs
-            raise Exception('Is this a walkoff?')
-
-        # Batter out trying to take an extra base
-        if not batter_runner and atbat.event in HITS_IN_PARK:
-            base = self.find_base_where_out_was_made(
-                self.players[atbat.batter].last, atbat.des)
-            atbat.add_runner(
-                Runner(atbat.batter, 0, base, atbat.event_num, out=True))
-            logging.debug('Runner inserted: batter is out at %d' % base)
-
-    def resolve_runners_3outs(self, atbat, outs_on_play):
-        """When the inning ends, the absences of a runner tag for the batter
-        does not necessarily mean that the batter is out. So the approach is
-        different. Look for runner outs in the description first. Not all empty
-        end attributes mean an out. Stranded runners are also empty."""
-        self.set_runner_out_bases(atbat, throw=False)
-
-        batter_out_base = self.find_base_where_out_was_made(
-            self.players[atbat.batter].last, atbat.des, throw=False)
-
-        # Batter out trying to take an extra base
-        if batter_out_base and (atbat.event in HITS_IN_PARK or
-                                self.batter_gets_a_hit(atbat)):
-            atbat.add_runner(
-                Runner(atbat.batter, 0, batter_out_base, atbat.event_num,
-                       out=True))
-            logging.debug('Runner inserted: batter is out at %d',
-                          batter_out_base)
-
-        runner_outs = sum([1 for r in atbat.runners if r.out])
-        if outs_on_play - runner_outs > 0:
-            # Batter should be out, so no need for runner tag
-            if atbat.scoring.result == 'on-base':
-                raise Exception('Batter should be out')
-        else:  # Batter not out, need to create stranded runner
-            if not batter_out_base and atbat.event in HITS_IN_PARK:
-                base = HITS_IN_PARK.index(atbat.event) + 1
-                atbat.add_runner(
-                    Runner(atbat.batter, 0, base, atbat.event_num))
-                logging.debug('Runner inserted: batter to %d', base)
-            elif not batter_out_base and atbat.event != 'Runner Out':
-                atbat.add_runner(Runner(atbat.batter, 0, 1, atbat.event_num))
-                logging.debug('Runner inserted: batter to 1st')
-
-        self.strand_remaining_runners(atbat)
-
-    def batter_gets_a_hit(self, atbat):
-        name = self.players[atbat.batter].last
-        return re.search('%s (?:singles|doubles|triples)' % name, atbat.des)
-
-    def set_runner_out_bases(self, atbat, throw=True):
-        runners_to_adjust = [r for r in atbat.runners if not r.end]
-        for runner in runners_to_adjust:
-            try:
-                runner.end = self.find_base_where_out_was_made(
-                    self.players[runner.id].last, atbat.des)
-                runner.out = True
-                logging.debug('Runner end adjusted: %s out at %d',
-                              self.players[runner.id], runner.end)
-            except Exception as e:
-                if throw:
-                    raise e
-
-    def strand_remaining_runners(self, atbat):
-        for runner in atbat.runners:
-            if not runner.end and not runner.out:
-                runner.end = runner.start
-
-    def display_atbat(self, atbat):
-        logging.debug('%d %s (%s)', atbat.pa_num, atbat.scoring.code,
-                      atbat.scoring.result)
-        logging.debug(atbat.des)
-        self.display_runners(atbat.mid_pa_runners)
-        if atbat.mid_pa_runners:
-            logging.debug('-----mid_pa runners above-----')
-        self.display_runners(atbat.runners)
-        logging.debug('%d out', atbat.outs)
-
-    def display_runners(self, runners):
-        for runner in runners:
-            insert = ''
-            if runner.out:
-                insert = ' (out)'
-            elif not runner.out and runner.end == 4:
-                insert = ' (scores)'
-            logging.debug('%s: %d -> %d%s', self.players[runner.id],
-                          runner.start, runner.end, insert)
+    def parse_player(self, id):
+        """Looks up name information for player id, adds to cache"""
+        if id not in self.players:
+            response = statsapi.get('person', {'personId': id})
+            # name = response['people'][0]['initLastName']
+            first = response['people'][0]['useName']
+            last = response['people'][0]['boxscoreName']
+            self.players[id] = Player(id, first, last)
 
     def hold_runners(self, active_runners, atbat):
         """The schema only represents runner movement. We need a record in each
         atbat even when the runners hold. This algorithm doesn't worry about
-        who the runners are (pinch runners gum up the system) and so it creates
-        dummy runners."""
+        who the runners are and so it creates dummy runners."""
         occupied_bases = [r.end for r in active_runners]
         assert len(occupied_bases) == len(set(occupied_bases))
 
@@ -179,38 +63,3 @@ class GameEnhancer:
         starting_bases = set([r.start for r in atbat.runners])
         for base in set(occupied_bases).difference(starting_bases):
             atbat.add_runner(Runner(0, base, base, atbat.event_num))
-
-    def get_mid_pa_end_base(self, atbat, runner_id):
-        return max([r.end for r in atbat.mid_pa_runners if r.id == runner_id])
-
-    def fix_mid_pa_runners(self, atbat):
-        """All runner tags in the middle of the plate appearance without an
-        ending base are out on the basepaths. This records the base and marks
-        the runner out. This is typically a caught stealing or pickoff."""
-        for runner in atbat.mid_pa_runners:
-            if not runner.end:
-                runner.end = self.find_base_where_out_was_made(
-                    self.players[runner.id].last,
-                    atbat.get_action_by_event_num(runner.event_num).des)
-                runner.out = True
-
-    def find_base_where_out_was_made(self, runner_last_name, des, throw=True):
-        """Given the last name of a runner and a description of the play, this
-        returns the base where that runner was put out."""
-        g = re.search(('%s %s %s' %
-                       (runner_last_name, OUTS_ON_BASES, LONG_BASE)), des)
-        if g:
-            return BASE_NUMBER[g.group(1)]
-
-        g = re.search(PICKS_OFF % (runner_last_name, LONG_BASE), des)
-        if g:
-            return BASE_NUMBER[g.group(1)]
-
-        if throw:
-            raise BaseNotFoundException(
-                'Cannot find base where %s was put out:\n%s' %
-                (runner_last_name, des))
-
-
-class BaseNotFoundException(Exception):
-    pass
--- gd2score/parse_game.py
+++ gd2score/parse_game.py
@@ -1,87 +1,113 @@
-import re
-import xml.etree.ElementTree as ET
+import statsapi
 
-from .models import Game, Inning, HalfInning, Action, AtBat, Runner
-
-BASES = ('1B', '2B', '3B')
+from .models import Game, Inning, HalfInning, Action, AtBat, Runner, Player
+from .enhance import GameEnhancer
+from .draw_scorecard import DrawScorecard
 
 
 class GameParser:
-    def parse(self, xml):
+    def parse(self, game_dict):
+        game_id = game_dict['game_id']
+
         game = Game()
+        # Used later for logo lookups
+        game.away = self.get_team_code(game_dict['away_id'])
+        game.home = self.get_team_code(game_dict['home_id'])
 
-        game_root = ET.fromstring(xml)
-        ind = game_root.attrib['ind']
-        if ind == 'I':
-            game.in_progress = True
+        active_inning = None
+        active_half = None
+        top_bottom = 'top'
 
-        for inning in game_root:
-            game.add_inning(self.parse_inning(inning))
+        pbp = statsapi.get('game_playByPlay', {'gamePk': game_id})
+        for play in pbp['allPlays']:
+            inning_num = int(play['about']['inning'])
+            if not active_inning or active_inning.num != inning_num:
+                active_inning = Inning(inning_num)
+                game.add_inning(active_inning)
+
+            if not active_half or top_bottom != play['about']['halfInning']:
+                top_bottom = play['about']['halfInning']
+                active_half = HalfInning()
+                active_inning.add_half(active_half)
+
+            ab = AtBat(int(play['atBatIndex'] + 1),
+                       int(play['atBatIndex'] + 1),
+                       int(play['matchup']['batter']['id']),
+                       play['result']['description'],
+                       play['result']['event'],
+                       int(play['matchup']['pitcher']['id']),
+                       int(play['count']['outs']),
+                       int(play['result']['homeScore']),
+                       int(play['result']['awayScore']))
+
+            self.parse_runners(ab, play)
+            active_half.add_atbat(ab)
+
         return game
 
-    def parse_inning(self, inning):
-        active_inning = Inning(float(inning.attrib['num']))
-        for half_inning in inning:
-            active_inning.add_half(self.parse_half_inning(half_inning))
-        return active_inning
+    def parse_runners(self, ab, play):
+        runner_index = set(play['runnerIndex'])
+        for i, runner in enumerate(play['runners']):
+            mvmt = runner['movement']
+            start = self.get_base(mvmt['start'])
+            end = self.get_base(mvmt['end'])
+            if not start and not end:
+                continue
+            if mvmt['outBase']:
+                end = self.get_base(mvmt['outBase'])
 
-    def parse_half_inning(self, half_inning):
-        half = HalfInning()
-        for event in half_inning:
-            if event.tag == 'atbat':
-                half.add_atbat(self.parse_atbat(event))
-            elif event.tag == 'action':
-                half.add_action(self.parse_action(event))
-            else:
-                raise Exception('Unknown event type')
-        return half
+            # Runners that advance multiple bases are listed for each base they
+            # advance. Only create one Runner object per person.
+            already_added = False
+            play_index = int(runner['details']['playIndex'])
+            runner_id = runner['details']['runner']['id']
+            for ab_runner in ab.runners:
+                if (ab_runner.id == runner_id and
+                    ab_runner.event_num == play_index):
+                        assert end > ab_runner.end
+                        ab_runner.end = end  # Increase end
+                        already_added = True
+                        break
 
-    def parse_atbat(self, atbat):
-        pa_num = int(atbat.attrib['num'])
-        event_num = int(atbat.attrib['event_num'])
-        batter = int(atbat.attrib['batter'])
-        des = re.sub('\s\s+', ' ', atbat.attrib['des'].strip())
-        event = atbat.attrib['event']
-        outs = int(atbat.attrib['o'])
-        home_score = int(atbat.attrib['home_team_runs'])
-        away_score = int(atbat.attrib['away_team_runs'])
-        pitcher = int(atbat.attrib['pitcher'])
+            if not already_added:
+                r = Runner(runner_id, start, end, play_index)
+                r.to_score = runner['details']['isScoringEvent']
+                r.out = mvmt['isOut']
 
-        active_atbat = AtBat(pa_num, event_num, batter, des, event,
-                             pitcher, outs,
-                             home_score=home_score, away_score=away_score)
+                if i in runner_index:
+                    ab.add_atbat_runner(r)
+                else:
+                    ab.add_mid_pa_runner(r)
 
-        for child in atbat:
-            if child.tag == 'runner':
-                active_atbat.add_runner(self.parse_runner(child))
-            elif child.tag in ['pitch', 'po']:
-                pass
-            else:
-                raise Exception('Unknown atbat type: %s' % child.tag)
-
-        return active_atbat
-
-    def parse_action(self, action):
-        event_num = int(action.attrib['event_num'])
-        event = action.attrib['event']
-        des = re.sub('\s+', ' ', action.attrib['des']).strip()
-        player_id = int(action.attrib['player'])
-        return Action(event_num, event, des, player_id)
-
-    def parse_runner(self, runner):
-        id = int(runner.attrib['id'])
-        start = self.parse_base(runner.attrib['start'])
-        end = self.parse_base(runner.attrib['end'])
-        event_num = int(runner.attrib['event_num'])
-
-        if runner.attrib.get('score') == 'T':  # Scores!
-            end = 4
-        return Runner(id, start, end, event_num)
-
-    def parse_base(self, base):
+    def get_base(self, base):
         if not base:
             return 0
-        elif base in BASES:
-            return int(base[0])
+        elif base == 'score':
+            return 4
         else:
-            raise Exception('Could not parse base: %s' % base)
+            return int(base[0])
+
+    def get_team_code(self, team_id):
+        team = statsapi.get('team', {'teamId': team_id})['teams'][0]
+        return team['teamCode']
+
+
+if __name__ == '__main__':
+    games = statsapi.schedule(start_date='10/26/2018')
+    game_dict = games[0]
+
+    game = GameParser().parse(game_dict)
+    GameEnhancer().execute(game)
+    drawing = DrawScorecard().draw(game)
+    drawing.saveas('test.svg')
+
+    for inning in game:
+        print(inning)
+        for half in inning:
+            print(half)
+            for atbat in half:
+                print(atbat)
+                for runner in atbat.mid_pa_runners:
+                    print(runner, 'mid-pa')
+                for runner in atbat.runners:
+                    print(runner)
