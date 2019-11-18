28a6c11eb607fd02211229dd3cc820491f43f3e8
John Brewer <brewerja@gmail.com>
2019-11-17 23:03:19 -0500

    Delete old tests, ignore *.svg

--- tests/__main__.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from .pinch_runners import TestPinchRunnerFixer
-from .mid_pa_runners import TestFixMidPaRunners
-from .parse_players import PlayerTest, PlayersParserTest
-from .enhance import TestGameEnhancer
-
-unittest.main()
